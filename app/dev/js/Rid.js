2b07e254eb528506f600a6858b16943774582e5b
Sutirtha-Marjit <sutirtha.social.marjit@gmail.com>
2015-12-22 22:20:52 +0530

    Multiple Rid test1

--- app/dev/js/Rid.js
+++ app/dev/js/Rid.js
@@ -1,6 +1,7 @@
 function Rid(ridConfigObject) {
 
 	var instance = this;
+	var active = true;
 	this.config = ridConfigObject;
 
 	var generateAudioElement = function () {
@@ -11,30 +12,45 @@ function Rid(ridConfigObject) {
 		audioEl.className = "hiddenAudioObj";
 		instance.config.wrapperObject.appendChild(audioEl);
 		sourceElement.setAttribute("src", instance.config.src);
-        instance.audioEl=audioEl;
+		instance.audioEl = audioEl;
+
 	};
-    
-    var setRidGraphics = function(){
-      instance.rid =  instance.config.rid;
-      instance.rid.addEventListener('mousedown',function(){          
-          instance.audioEl.play();
-      });
-      
-      instance.rid.addEventListener('mouseup',function(){          
-          instance.audioEl.pause();
-      });
-    };
 
-	this.play = function () {};
+	var setRidGraphics = function () {
+		instance.rid = instance.config.rid;
+		instance.rid.addEventListener('click', function () {
+			instance.play();
+		});
 
-	this.deactivate = function () {};
+	};
 
-	this.activate = function () {};
+	this.play = function () {
+		if (active) {
+			instance.audioEl.pause();
+			instance.audioEl.currentTime = 0;
+			instance.audioEl.play();
+			return {
+				result : instance.config.src + " played"
+			};
+		}
+
+		return {
+			result : "Rid deactivated"
+		};
+	};
+
+	this.deactivate = function () {
+		active = false;
+	};
+
+	this.activate = function () {
+		active = true;
+	};
 
 	var init = function () {
 		console.log("Rid initiated");
 		generateAudioElement();
-        setRidGraphics();
+		setRidGraphics();
 	};
 
 	init();
--- app/dev/js/app.js
+++ app/dev/js/app.js
@@ -1,6 +1,14 @@
 var rid = new Rid({
     wrapperObject:document.getElementById('machineWrapper'),
     name:"testRid",
-    src:"audio/Sleep Away.mp3",
+    src:"audio/1A.mp3",
     rid:document.getElementById("rid")    
+});
+
+
+var rid2 = new Rid({
+    wrapperObject:document.getElementById('machineWrapper'),
+    name:"testRid2",
+    src:"audio/2F.mp3",
+    rid:document.getElementById("rid2")    
 });
